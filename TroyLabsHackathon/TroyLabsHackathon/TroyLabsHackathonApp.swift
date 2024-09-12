af2e0de00635a05239e8153f2fc2c330911877f8
Joshua Jenkins <2004joshuaj@gmail.com>
2024-09-11 20:15:18 -0700

    initial push

---
+++ TroyLabsHackathon/TroyLabsHackathon/TroyLabsHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TroyLabsHackathonApp.swift
+//  TroyLabsHackathon
+//
+//  Created by Joshua Jenkins on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TroyLabsHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
